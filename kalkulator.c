d3f5a63bc85113067215413bee90ce6665bda25b
Maciej <m.sobczyk117@studms.ug.edu.pl>
2020-12-11 16:30:26 +0100

    Podstawa programu

---
+++ kalkulator.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void dodawanie();
+void odejmowanie();
+void mnozenie();
+void dzielenie();
+
+
+int main(){
+
+    unsigned int wybor;
+    printf("------Kalkulator------\n Wybierz:\n 1) Dodawanie\n 2) Odejmowanie\n 3) Mnożenie\n 4) Dzielenie\n Wybierz:");
+    scanf("%u",&wybor);
+    printf("\n");
+
+    switch (wybor)
+    {
+    case 1:
+        /* code */
+        break;
+
+    case 2:
+        /* code */
+        break;
+    
+    case 3:
+        /* code */
+        break;
+    
+    case 4:
+        /* code */
+        break;
+    
+    default:
+    printf("Nieprawidlowa liczba");
+        break;
+    }
+
+
+    return 0;
+
+}
+
+
+void dodawanie(){
+
+
+}
+void odejmowanie(){
+
+
+}
+void mnozenie(){
+
+
+}
+void dzielenie(){
+
+    
+}
